e5141ba12851f6209a5ec99cd3dbd7bfbb427adb
hoang.ausway <hoang.ausway@gmail.com>
2020-11-22 18:50:35 +1100

    cleanup config

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,47 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
-module.exports = (_, argv) => ({
-  output: {
-    publicPath:
-      argv.mode === 'development'
-        ? 'http://localhost:8080/'
-        : 'https://gameoflife-motionary.netlify.app/'
-  },
+const output = mode => ({
+  publicPath:
+    mode === 'development'
+      ? 'http://localhost:8080/'
+      : 'https://gameoflife-motionary.netlify.app/'
+})
 
-  resolve: {
-    extensions: ['.jsx', '.js', '.json']
-  },
+const resolve = { extensions: ['.jsx', '.js', '.json'] }
+const devServer = { port: 8080 }
 
-  devServer: {
-    port: 8080
-  },
+const ruleCss = { test: /\.css$/i, use: ['style-loader', 'css-loader'] }
+const ruleBabel = {
+  test: /\.jsx?$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
+  }
+}
+const rules = [ruleCss, ruleBabel]
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react']
-        }
-      }
-    ]
+const moduleFederationPlugin = new ModuleFederationPlugin({
+  name: 'gameoflife',
+  filename: 'remoteEntry.js',
+  remotes: {},
+  exposes: {
+    './Gol': './src/components/GOL'
   },
+  shared: require('./package.json').dependencies
+})
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'gameoflife',
-      filename: 'remoteEntry.js',
-      remotes: {},
-      exposes: {
-        './Gol': './src/components/GOL'
-      },
-      shared: require('./package.json').dependencies
-    }),
-    new HtmlWebPackPlugin({
-      template: './src/index.html'
-    })
-  ]
+const htmlWebPackPlugin = new HtmlWebPackPlugin({
+  title: 'Game Of Life',
+  template: './src/index.html'
+})
+
+module.exports = (_, { mode }) => ({
+  output: output(mode),
+  resolve,
+  devServer,
+  module: { rules },
+  plugins: [moduleFederationPlugin, htmlWebPackPlugin]
 })

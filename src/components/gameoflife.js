8618199276bb2adc3d6ff22b402cccd6017d2f7f
hoang.ausway <hoang.ausway@gmail.com>
2020-11-25 07:46:35 +1100

    celanup gameoflife.js

--- src/components/gameoflife.js
+++ src/components/gameoflife.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-
 import { useMediaQuery } from 'react-responsive'
-
 import Gol from './gol'
 import '../styles/index.css'
 
